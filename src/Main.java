a435947bc0976df8d913299cce7c09246e10e825
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2022-04-28 10:29:17 +0000

    Feature: Añadida clase de utilidades aleatorias

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import com.alberto.utils.SuperMarketUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 0; i < 5; i++) {
+            System.out.println(SuperMarketUtils.generarNombreAleatorio());
+            System.out.println(SuperMarketUtils.generarProductoAleatorio());
+        }
     }
 }
--- src/com/alberto/utils/SuperMarketUtils.java
+++ src/com/alberto/utils/SuperMarketUtils.java
@@ -1,5 +1,37 @@
 package com.alberto.utils;
 
 public class SuperMarketUtils {
+    private static final String[] NOMBRES_PERSONA = {
+            "Alberto",
+            "Lucrecia",
+            "Montse",
+            "Montxo",
+            "Lionel",
+            "Francisco",
+            "Nicolás",
+            "Yolanda",
+            "Pedro",
+            "Alexis"
+    };
 
+    public static final String[] NOMBRES_PRODUCTOS = {
+            "Leche",
+            "Huevos",
+            "Galletas",
+            "Salami",
+            "Chorizo",
+            "Pan",
+            "Queso",
+            "Agua",
+            "Carne",
+            "Millo"
+    };
+
+    public static String generarNombreAleatorio() {
+        return NOMBRES_PERSONA[(int) (Math.random() * NOMBRES_PERSONA.length)];
+    }
+
+    public static String generarProductoAleatorio() {
+        return NOMBRES_PRODUCTOS[(int) (Math.random() * NOMBRES_PRODUCTOS.length)];
+    }
 }
